c4f605c46c5beeb0857e11e053960de2465d1057
Kevin <kevingtxz@gmail.com>
2020-09-03 21:45:57 -0300

    Updating 2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from time import sleep
 import pandas as pd
 from xlsxwriter import Workbook
@@ -8,27 +9,27 @@ import datetime
 stores = ['https://www.americanas.com.br/', 'https://www.kabum.com.br/', 'https://www.pichau.com.br/', 'https://www.terabyteshop.com.br/', 'https://www.shoptime.com.br/', 'https://www.magazineluiza.com.br/']
 
 # Reading a file with pandas
-list_products = pd.read_excel(r"C:\ws\Web Scraping - Compare prices of video cards\list_links_americanas.xlsx")
+list_products = pd.read_excel(r"C:\ws\WebScrapingComparepricesfromBrazil\list_products.xlsx")
 # Index count
 index = 0
 
 # List to put prices as a dataframe with Pandas
 prices = []
             
-# var to save the best price of each store            
-best_price = 0
-
 # Class to handle the web driver
 class driver_handler:
-    def __init__(self, list_links_americanas):
+    def __init__(self, list_products):
         self.driver = webdriver.Firefox()
-    
-#     def find_prices_americanas(self):
-#         count = 0
-#         prices = []
-#         while(isinstance(list_links_americanas['Link'][count], str)):        
-#             self.driver.get(list_links_americanas['Link'][count])
-#             sleep(2)
+    def find_prices_americanas(self):
+        if(isinstance(list_products['Product'][index], str)):
+            self.driver.get(stores[0])
+            sleep(2)
+            self.driver.find_element_by_id("h_search").send_keys(list_products['Product'][index])
+            sleep(1)
+            self.driver.find_element_by_id("h_search").send_keys(Keys.RETURN)
+driver = driver_handler(list_products)
+driver.find_prices_americanas()
+
 #             # I used replace() to handle text data, because in Brazil, we use don't use dot and the Excel can't understand R$ as a number
 #             prices.append(self.driver.find_element_by_xpath("//span[contains(text(), 'R$')]").text.replace("R$","").replace(".",""))
 #             count += 1
@@ -46,9 +47,7 @@ class driver_handler:
 # workbook  = Workbook('list_products.xlsx')
 # worksheet = workbook.add_worksheet()
 # writer.save()
-        def find_best_price_americanas(self, product):
-            self.driver.get(stores[index])
+#         def find_best_price_americanas(self, product):
+#             self.driver.get(stores[index])
             #id="h_search"
-
-
-pd.DataFrame({datetime.datetime.now(): prices})
+# pd.DataFrame({datetime.datetime.now(): prices})
